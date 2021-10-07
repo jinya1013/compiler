73246bbb2f32643d0e40cfc5beb97849296f06de
fushigu <fushigu5@gmail.com>
2021-10-07 23:01:43 +0900

    add Syntax.position to Let in asm.ml

--- x86/asm.mli
+++ x86/asm.mli
@@ -1,7 +1,7 @@
 type id_or_imm = V of Id.t | C of int
 type t =
   | Ans of exp * Syntax.position
-  | Let of (Id.t * Type.t) * exp * t
+  | Let of (Id.t * Type.t) * exp * t * Syntax.position
 and exp =
   | Nop
   | Set of int
