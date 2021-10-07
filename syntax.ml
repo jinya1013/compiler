e96ae95dd22670b8caf25ccad799de2ea898f258
jinya1013 <gab102manda100@gmail.com>
2021-10-08 01:51:31 +0900

    outputの形式を変更

--- syntax.ml
+++ syntax.ml
@@ -1,4 +1,4 @@
-type t = (* MinCaml¤Î¹½Ê¸¤òÉ½¸½¤¹¤ë¥Ç¡¼¥¿·¿ (caml2html: syntax_t) *)
+type t = (* MinCamlã®æ§‹æ–‡ã‚’è¡¨ç¾ã™ã‚‹ãƒ‡ãƒ¼ã‚¿å‹ (caml2html: syntax_t) *)
   | Unit
   | Bool of bool
   | Int of int
@@ -26,189 +26,287 @@ type t = (* MinCaml¤Î¹½Ê¸¤òÉ½¸½¤¹¤ë¥Ç¡¼¥¿·¿ (caml2html: syntax_t) *)
   | Put of t * t * t
 and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
 
+let output_tab outchan depth = 
+  let rec _output_tab iter = 
+    match iter with
+    | 0 -> ()
+    | _ -> 
+    (
+      output_string outchan "\t";
+      _output_tab (iter-1)
+    )
+    in _output_tab depth
 
-let rec output_syntax outchan s = 
+
+let rec output_syntax outchan s depth = 
+(* 
+    ä¸ãˆã‚‰ã‚ŒãŸå¼sã‚’ãƒãƒ£ãƒãƒ«outchanã«å‡ºåŠ›ã™ã‚‹.
+
+    Args
+        outchan : out_channel
+          å‡ºåŠ›å…ˆã®ãƒãƒ£ãƒ³ãƒãƒ«
+        s : Syntax.t
+          å‡ºåŠ›ã™ã‚‹å¼
+
+    Returns
+        retval : unit
+          ãªã—            
+*)
   match s with
   | Unit -> ()
   | Bool b -> 
   (
-    if b then output_string outchan "true"
-    else output_string outchan "false"
+    if b then output_string outchan "B00L TRUE\n"
+    else output_string outchan "BOOL FALSE\n"
   )
-  | Int i -> output_string outchan (string_of_int i)
-  | Float f -> output_string outchan (string_of_float f)
+  | Int i -> output_string outchan ("INT " ^ (string_of_int i) ^ "\n")
+  | Float f -> output_string outchan ("FLOAT " ^ (string_of_float f) ^ "\n")
   | Not t ->
   (
-    output_string outchan "Not( ";
-    output_syntax outchan t;
-    output_string outchan " )"
+    output_string outchan "NOT\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t (depth + 1);
+    output_string outchan "\n";
   )
   | Neg t ->
   (
-    output_string outchan "Neg( ";
-    output_syntax outchan t;
-    output_string outchan " )"
+    output_string outchan "NEG\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Add (t1, t2) ->
   (
-    output_string outchan "Add( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "ADD\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Sub (t1, t2) ->
   (
-    output_string outchan "Sub( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "SUB\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+
+
   )
   | FNeg t ->
   (
-    output_string outchan "FNeg( ";
-    output_syntax outchan t;
-    output_string outchan " )"
+    output_string outchan "FNEG\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | FAdd (t1, t2) ->
   (
-    output_string outchan "FAdd( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "FADD\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | FSub (t1, t2) ->
   (
-    output_string outchan "FSub( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "FSUB\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | FMul (t1, t2) ->
   (
-    output_string outchan "FMul( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "FMUL\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | FDiv (t1, t2) ->
   (
-    output_string outchan "FDiv( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "FDIV\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Eq (t1, t2) ->
   (
-    output_string outchan "Eq( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "EQ\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | LE (t1, t2) ->
   (
-    output_string outchan "LE( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "LE\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | If (c, t1, t2) ->
   (
-    output_string outchan "If( ";
-    output_syntax outchan c;
-    output_string outchan ", ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "IF\n\t";
+    output_tab outchan depth;
+    output_syntax outchan c (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Let (t1, t2, t3) ->
   (
-    output_string outchan "Let( ";
+    output_string outchan "LET\n\t";
+    output_tab outchan depth;
     Id.output_id outchan (fst t1);
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan ", ";
-    output_syntax outchan t3;
-    output_string outchan " )"
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t3 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
-  | Var (x) -> Id.output_id outchan x
+  | Var (x) -> 
+  (
+    output_string outchan "VAR\n\t";
+    output_tab outchan depth;
+    Id.output_id outchan x;
+    output_string outchan "\n";
+    output_tab outchan depth;
+  )
   | LetRec ({ name = f; args = a; body = b }, t) ->
   (
-    output_string outchan "LetRec( { name = ";
+    output_string outchan "LETREC\n";
+    output_tab outchan depth;
+    output_string outchan "{\n";
+    output_tab outchan depth;
+    output_string outchan "name = ";
     Id.output_id outchan (fst f);
-    output_string outchan ", args = ( ";
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_string outchan "args = \n";
+    output_tab outchan depth;
+    output_string outchan "(\n";
+    output_tab outchan depth;
     Id.output_id_list outchan (fst (List.split a));
-    output_string outchan " ), body = ";
-    output_syntax outchan b;
-    output_string outchan " }, ";
-    output_syntax outchan t;
-    output_string outchan " )";
+    output_string outchan " )\n";
+    output_tab outchan depth;
+    output_string outchan "body = ";
+    output_syntax outchan b (depth + 1);
+    output_string outchan " }\n";
+    output_syntax outchan t (depth + 1); 
+    output_string outchan "\n";
   )
 
   | App (t, ts) ->
   (
-    output_string outchan "App( ";
-    output_syntax outchan t;
-    output_syntax_list outchan ts;
-    output_string outchan " )"
+    output_string outchan "APP\n\t";
+    output_syntax outchan t (depth + 1);
+    output_syntax_list outchan ts (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Tuple (ts) ->
   (
-    output_string outchan "( ";
-    output_syntax_list outchan ts;
-    output_string outchan " )";
+    output_string outchan "(\n\t";
+    output_tab outchan depth;
+    output_syntax_list outchan ts (depth + 1);
+    output_tab outchan depth;
+    output_string outchan ")\n";
+    output_tab outchan depth;
   )
   | LetTuple (t1s, t2, t3) ->
   (
-    output_string outchan "Let( ( ";
+    output_string outchan "LET\n";
+    output_tab outchan depth;
+    output_string outchan "(\n";
+    output_tab outchan depth;
     Id.output_id_list outchan (fst (List.split t1s));
-    output_string outchan " ), ";
-    output_syntax outchan t2;
-    output_string outchan ", ";
-    output_syntax outchan t3;
-    output_string outchan " )";
+    output_string outchan ")\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t3 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Array (t1, t2) ->
   (
-    output_string outchan "Array( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "ARRAY\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Get (t1, t2) ->
   (
-    output_string outchan "Get( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan " )"
+    output_string outchan "GET\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
   | Put (t1, t2, t3) ->
   (
-    output_string outchan "Put( ";
-    output_syntax outchan t1;
-    output_string outchan ", ";
-    output_syntax outchan t2;
-    output_string outchan ", ";
-    output_syntax outchan t3;
-    output_string outchan " )"
+    output_string outchan "PUT\n\t";
+    output_tab outchan depth;
+    output_syntax outchan t1 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t2 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t3 (depth + 1);
+    output_string outchan "\n";
+    output_tab outchan depth;
   )
-and output_syntax_list outchan ts = 
+and output_syntax_list outchan ts depth = 
   let f t = 
-    output_string outchan ", ";
-    output_syntax outchan t in
-  output_syntax outchan (List.hd ts);
+    output_string outchan "\n";
+    output_tab outchan depth;
+    output_syntax outchan t (depth + 1) in
+  output_syntax outchan (List.hd ts) (depth + 1);
   List.iter f (List.tl ts)
-    
-
-
-
