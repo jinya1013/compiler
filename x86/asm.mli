42925a541f96e441d2d27321604092744e56d57f
fushigu <fushigu5@gmail.com>
2021-10-07 22:41:24 +0900

    delete Syntax.position from asm.mli

--- x86/asm.mli
+++ x86/asm.mli
@@ -3,35 +3,35 @@ type t =
   | Ans of exp
   | Let of (Id.t * Type.t) * exp * t
 and exp =
-  | Nop of Syntax.position
-  | Set of int * Syntax.position
-  | SetL of Id.l * Syntax.position
-  | Mov of Id.t * Syntax.position
-  | Neg of Id.t * Syntax.position
-  | Add of Id.t * id_or_imm * Syntax.position
-  | Sub of Id.t * id_or_imm * Syntax.position
-  | Ld of Id.t * id_or_imm * int * Syntax.position
-  | St of Id.t * Id.t * id_or_imm * int * Syntax.position
-  | FMovD of Id.t * Syntax.position
-  | FNegD of Id.t * Syntax.position
-  | FAddD of Id.t * Id.t * Syntax.position
-  | FSubD of Id.t * Id.t * Syntax.position
-  | FMulD of Id.t * Id.t * Syntax.position
-  | FDivD of Id.t * Id.t * Syntax.position
-  | LdDF of Id.t * id_or_imm * int * Syntax.position
-  | StDF of Id.t * Id.t * id_or_imm * int * Syntax.position
-  | Comment of string * Syntax.position
+  | Nop
+  | Set of int
+  | SetL of Id.l
+  | Mov of Id.t
+  | Neg of Id.t
+  | Add of Id.t * id_or_imm
+  | Sub of Id.t * id_or_imm
+  | Ld of Id.t * id_or_imm * int
+  | St of Id.t * Id.t * id_or_imm * int
+  | FMovD of Id.t
+  | FNegD of Id.t
+  | FAddD of Id.t * Id.t
+  | FSubD of Id.t * Id.t
+  | FMulD of Id.t * Id.t
+  | FDivD of Id.t * Id.t
+  | LdDF of Id.t * id_or_imm * int
+  | StDF of Id.t * Id.t * id_or_imm * int 
+  | Comment of string
   (* virtual instructions *)
-  | IfEq of Id.t * id_or_imm * t * t * Syntax.position
-  | IfLE of Id.t * id_or_imm * t * t * Syntax.position
-  | IfGE of Id.t * id_or_imm * t * t * Syntax.position
-  | IfFEq of Id.t * Id.t * t * t * Syntax.position
-  | IfFLE of Id.t * Id.t * t * t * Syntax.position
+  | IfEq of Id.t * id_or_imm * t * t
+  | IfLE of Id.t * id_or_imm * t * t
+  | IfGE of Id.t * id_or_imm * t * t
+  | IfFEq of Id.t * Id.t * t * t
+  | IfFLE of Id.t * Id.t * t * t
   (* closure address, integer arguments, and float arguments *)
-  | CallCls of Id.t * Id.t list * Id.t list * Syntax.position
-  | CallDir of Id.l * Id.t list * Id.t list * Syntax.position
-  | Save of Id.t * Id.t * Syntax.position (* レジスタ変数の値をスタック変数へ保存 *)
-  | Restore of Id.t * Syntax.position (* スタック変数から値を復元 *)
+  | CallCls of Id.t * Id.t list * Id.t list
+  | CallDir of Id.l * Id.t list * Id.t list
+  | Save of Id.t * Id.t (* レジスタ変数の値をスタック変数へ保存 *)
+  | Restore of Id.t (* スタック変数から値を復元 *)
 type fundef = { name : Id.l; args : Id.t list; fargs : Id.t list; body : t; ret : Type.t }
 type prog = Prog of (Id.l * float) list * fundef list * t
 
