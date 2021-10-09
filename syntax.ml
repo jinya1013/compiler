aff71fd591735fd4573f7a934cff346892f331b2
fushigu <fushigu5@gmail.com>
2021-10-09 23:01:36 +0900

    kadai2 fin!!!!

--- syntax.ml
+++ syntax.ml
@@ -1,55 +1,59 @@
+type pos = int
 type t = (* MinCamlの構文を表現するデータ型 (caml2html: syntax_t) *)
-  | Unit
-  | Bool of bool
-  | Int of int
-  | Float of float
-  | Not of t
-  | Neg of t
-  | Add of t * t
-  | Sub of t * t
-  | FNeg of t
-  | FAdd of t * t
-  | FSub of t * t
-  | FMul of t * t
-  | FDiv of t * t
-  | Eq of t * t
-  | LE of t * t
-  | If of t * t * t
-  | Let of (Id.t * Type.t) * t * t
-  | Var of Id.t
-  | LetRec of fundef * t
-  | App of t * t list
-  | Tuple of t list
-  | LetTuple of (Id.t * Type.t) list * t * t
-  | Array of t * t
-  | Get of t * t
-  | Put of t * t * t
+  | Unit of pos
+  | Bool of bool * pos 
+  | Int of int * pos 
+  | Float of float * pos 
+  | Not of t * pos 
+  | Neg of t * pos 
+  | Add of t * t * pos 
+  | Sub of t * t * pos 
+  | FNeg of t * pos 
+  | FAdd of t * t * pos 
+  | FSub of t * t * pos 
+  | FMul of t * t * pos 
+  | FDiv of t * t * pos 
+  | Eq of t * t * pos 
+  | LE of t * t * pos 
+  | If of t * t * t * pos 
+  | Let of (Id.t * Type.t) * t * t * pos 
+  | Var of Id.t * pos 
+  | LetRec of fundef * t * pos 
+  | App of t * t list * pos
+  | Tuple of t list * pos
+  | LetTuple of (Id.t * Type.t) list * t * t * pos
+  | Array of t * t * pos
+  | Get of t * t * pos
+  | Put of t * t * t * pos
 and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
+
+
+let top_pos:pos = 0
 
 
 let rec output_syntax outchan s = 
   match s with
-  | Unit -> ()
-  | Bool b -> 
+  | Unit(p)  -> ()
+  | Bool(b,p) -> 
   (
     if b then output_string outchan "true"
     else output_string outchan "false"
   )
-  | Int i -> output_string outchan (string_of_int i)
-  | Float f -> output_string outchan (string_of_float f)
-  | Not t ->
+  | Int(i,p) -> output_string outchan (string_of_int i)
+  | Float (f,p) -> output_string outchan (string_of_float f)
+  | Not (t,p) ->
   (
     output_string outchan "Not( ";
     output_syntax outchan t;
     output_string outchan " )"
   )
-  | Neg t ->
+  | Neg (t,p) ->
   (
     output_string outchan "Neg( ";
     output_syntax outchan t;
     output_string outchan " )"
   )
-  | Add (t1, t2) ->
+  | Add (t1, t2,p) ->
   (
     output_string outchan "Add( ";
     output_syntax outchan t1;
@@ -57,7 +61,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | Sub (t1, t2) ->
+  | Sub (t1, t2, p) ->
   (
     output_string outchan "Sub( ";
     output_syntax outchan t1;
@@ -65,13 +69,13 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | FNeg t ->
+  | FNeg (t,p) ->
   (
     output_string outchan "FNeg( ";
     output_syntax outchan t;
     output_string outchan " )"
   )
-  | FAdd (t1, t2) ->
+  | FAdd (t1, t2, p) ->
   (
     output_string outchan "FAdd( ";
     output_syntax outchan t1;
@@ -79,7 +83,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | FSub (t1, t2) ->
+  | FSub (t1, t2, p) ->
   (
     output_string outchan "FSub( ";
     output_syntax outchan t1;
@@ -87,7 +91,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | FMul (t1, t2) ->
+  | FMul (t1, t2, p) ->
   (
     output_string outchan "FMul( ";
     output_syntax outchan t1;
@@ -95,7 +99,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | FDiv (t1, t2) ->
+  | FDiv (t1, t2, p) ->
   (
     output_string outchan "FDiv( ";
     output_syntax outchan t1;
@@ -103,7 +107,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | Eq (t1, t2) ->
+  | Eq (t1, t2, p) ->
   (
     output_string outchan "Eq( ";
     output_syntax outchan t1;
@@ -111,7 +115,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | LE (t1, t2) ->
+  | LE (t1, t2, p) ->
   (
     output_string outchan "LE( ";
     output_syntax outchan t1;
@@ -119,7 +123,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | If (c, t1, t2) ->
+  | If (c, t1, t2, p) ->
   (
     output_string outchan "If( ";
     output_syntax outchan c;
@@ -129,7 +133,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | Let (t1, t2, t3) ->
+  | Let (t1, t2, t3, p) ->
   (
     output_string outchan "Let( ";
     Id.output_id outchan (fst t1);
@@ -139,8 +143,8 @@ let rec output_syntax outchan s =
     output_syntax outchan t3;
     output_string outchan " )"
   )
-  | Var (x) -> Id.output_id outchan x
-  | LetRec ({ name = f; args = a; body = b }, t) ->
+  | Var (x, p) -> Id.output_id outchan x
+  | LetRec ({ name = f; args = a; body = b }, t, p) ->
   (
     output_string outchan "LetRec( { name = ";
     Id.output_id outchan (fst f);
@@ -153,20 +157,20 @@ let rec output_syntax outchan s =
     output_string outchan " )";
   )
 
-  | App (t, ts) ->
+  | App (t, ts, p) ->
   (
     output_string outchan "App( ";
     output_syntax outchan t;
     output_syntax_list outchan ts;
     output_string outchan " )"
   )
-  | Tuple (ts) ->
+  | Tuple (ts, p) ->
   (
     output_string outchan "( ";
     output_syntax_list outchan ts;
     output_string outchan " )";
   )
-  | LetTuple (t1s, t2, t3) ->
+  | LetTuple (t1s, t2, t3, p) ->
   (
     output_string outchan "Let( ( ";
     Id.output_id_list outchan (fst (List.split t1s));
@@ -176,7 +180,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t3;
     output_string outchan " )";
   )
-  | Array (t1, t2) ->
+  | Array (t1, t2, p) ->
   (
     output_string outchan "Array( ";
     output_syntax outchan t1;
@@ -184,7 +188,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | Get (t1, t2) ->
+  | Get (t1, t2, p) ->
   (
     output_string outchan "Get( ";
     output_syntax outchan t1;
@@ -192,7 +196,7 @@ let rec output_syntax outchan s =
     output_syntax outchan t2;
     output_string outchan " )"
   )
-  | Put (t1, t2, t3) ->
+  | Put (t1, t2, t3, p) ->
   (
     output_string outchan "Put( ";
     output_syntax outchan t1;
