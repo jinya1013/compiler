5384abbedb59f510cec3dc6e502bc779f7d61752
jinya1013 <gab102manda100@gmail.com>
2021-11-13 19:37:24 +0900

    Makefileの変更

--- type.ml
+++ type.ml
@@ -16,19 +16,34 @@ let rec output_type outchan s =
   | Bool -> output_string outchan "Bool"
   | Int -> output_string outchan "Int"
   | Float -> output_string outchan "Float"
-  | Fun(ts, t) -> 
+  | Fun(t1s, t2) -> 
   (
-    output_string outchan "Fun( ";
-    output_type_list outchan ts;
-    output_string outchan ", ";
-    output_type outchan t;
-    output_string outchan " )"
+    output_string outchan " Fun ( (";
+    (
+      match t1s with
+      | th :: tt ->
+      (
+        output_type outchan th;
+        List.iter (fun t -> output_string outchan ", "; output_type outchan t) tt; 
+      )
+      | _ -> ()
+    );
+    output_string outchan ") -> ";
+    output_type outchan t2;
+    output_string outchan " )";
   )
   | Tuple(ts) -> 
   (
-    output_string outchan "( ";
-    output_type_list outchan ts;
-    output_string outchan " )"
+    match ts with
+    | th :: tt -> 
+    (
+      output_string outchan "(";
+      output_type outchan th;
+      List.iter (fun t -> output_string outchan " * "; output_type outchan t) tt;
+      output_string outchan ")";
+
+    )
+    | _ -> ()
   )
   | Array(t) ->
   (
@@ -39,7 +54,11 @@ let rec output_type outchan s =
   | Var(tref) ->
   (
     output_string outchan "Var( ";
-    output_type outchan (Option.get !tref);
+    (
+      match  !tref with
+    | None -> output_string outchan "None"
+    | Some(x) -> output_type outchan x
+    );
     output_string outchan " )";
   )
   and output_type_list outchan ts = 
@@ -48,4 +67,5 @@ let rec output_type outchan s =
       output_type outchan t in
     output_type outchan (List.hd ts);
     List.iter f ts
+
 
